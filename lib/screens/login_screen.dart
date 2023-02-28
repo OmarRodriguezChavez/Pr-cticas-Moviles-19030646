440f6297734ca277a17a2ab1e82eb2be44a73a58
OmarRodriguezChavez <19030646@itcelaya.edu.mx>
2023-02-28 00:17:26 -0600

    Práctica 2. Login Responsive - Clase 27-02-2023

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:psmna10/widgets/loading_modal_widget.dart';
 import 'package:social_login_buttons/social_login_buttons.dart';
+import '../responsive.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -59,23 +60,166 @@ class _LoginScreenState extends State<LoginScreen> {
           });
         });
 
-    return Scaffold(
+return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: Stack(
+      body: Responsive(
+        mobile: Mobile(
+          btnEmail: btnEmail,
+          isLoading: isLoading,
+          imgLogo: imgLogo,
+          btnface: btnface,
+          btngit: btngit,
+          btngoogle: btngoogle,
+          spaceHorizont: spaceHorizont,
+          txtPass: txtPass,
+          txtRegister: txtRegister,
+          txtEmail: txtEmail,
+        ),
+        tablet: Tablet(
+          btnEmail: btnEmail,
+          isLoading: isLoading,
+          imgLogo: imgLogo,
+          btnface: btnface,
+          btngit: btngit,
+          btngoogle: btngoogle,
+          spaceHorizont: spaceHorizont,
+          txtPass: txtPass,
+          txtRegister: txtRegister,
+          txtEmail: txtEmail,
+        ),
+        desktop: Desktop(
+          btnEmail: btnEmail,
+          isLoading: isLoading,
+          imgLogo: imgLogo,
+          btnface: btnface,
+          btngit: btngit,
+          btngoogle: btngoogle,
+          spaceHorizont: spaceHorizont,
+          txtPass: txtPass,
+          txtRegister: txtRegister,
+          txtEmail: txtEmail,
+        ),
+      ),
+    );
+  }
+}
+
+class Mobile extends StatelessWidget {
+  const Mobile(
+      {super.key,
+      required this.btnEmail,
+      required this.isLoading,
+      required this.imgLogo,
+      required this.txtEmail,
+      required this.btnface,
+      required this.btngit,
+      required this.btngoogle,
+      required this.spaceHorizont,
+      required this.txtPass,
+      required this.txtRegister});
+  final SizedBox spaceHorizont;
+  final TextFormField txtPass;
+  final SocialLoginButton btngit;
+  final SocialLoginButton btngoogle;
+  final SocialLoginButton btnface;
+  final SocialLoginButton btnEmail;
+  final Padding txtRegister;
+  final bool isLoading;
+  final Image imgLogo;
+  final TextFormField txtEmail;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  opacity: .4,
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/fondo.jpg'))),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    txtEmail,
+                    spaceHorizont,
+                    txtPass,
+                    spaceHorizont,
+                    btnEmail,
+                    spaceHorizont,
+                    btngoogle,
+                    spaceHorizont,
+                    btnface,
+                    spaceHorizont,
+                    btngit,
+                    spaceHorizont,
+                    txtRegister
+                  ],
+                ),
+                Positioned(
+                  top: 100,
+                  child: imgLogo,
+                )
+              ],
+            ),
+          ),
+        ),
+        isLoading ? const LoadingModalWidget() : Container()
+      ],
+    );
+  }
+}
+
+class Tablet extends StatelessWidget {
+  const Tablet(
+      {super.key,
+      required this.btnEmail,
+      required this.isLoading,
+      required this.imgLogo,
+      required this.txtEmail,
+      required this.btnface,
+      required this.btngit,
+      required this.btngoogle,
+      required this.spaceHorizont,
+      required this.txtPass,
+      required this.txtRegister});
+  final SizedBox spaceHorizont;
+  final TextFormField txtPass;
+  final SocialLoginButton btngit;
+  final SocialLoginButton btngoogle;
+  final SocialLoginButton btnface;
+  final Padding txtRegister;
+  final SocialLoginButton btnEmail;
+  final bool isLoading;
+  final Image imgLogo;
+  final TextFormField txtEmail;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          opacity: .4,
+          fit: BoxFit.cover,
+          image: AssetImage('assets/fondo.jpg'),
+        ),
+      ),
+      child: Row(
         children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    opacity: .4,
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/fondo.jpg'))),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
-                alignment: Alignment.topCenter,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
+          Expanded(child: imgLogo),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 300.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       txtEmail,
                       spaceHorizont,
@@ -92,15 +236,81 @@ class _LoginScreenState extends State<LoginScreen> {
                       txtRegister
                     ],
                   ),
-                  Positioned(
-                    top: 100,
-                    child: imgLogo,
-                  )
-                ],
-              ),
+                ),
+              ],
             ),
           ),
-          isLoading ? const LoadingModalWidget() : Container()
+        ],
+      ),
+    );
+  }
+}
+
+class Desktop extends StatelessWidget {
+  const Desktop(
+      {super.key,
+      required this.btnEmail,
+      required this.isLoading,
+      required this.imgLogo,
+      required this.txtEmail,
+      required this.btnface,
+      required this.btngit,
+      required this.btngoogle,
+      required this.spaceHorizont,
+      required this.txtPass,
+      required this.txtRegister});
+  final SizedBox spaceHorizont;
+  final TextFormField txtPass;
+  final SocialLoginButton btngit;
+  final SocialLoginButton btngoogle;
+  final SocialLoginButton btnface;
+  final Padding txtRegister;
+  final SocialLoginButton btnEmail;
+  final bool isLoading;
+  final Image imgLogo;
+  final TextFormField txtEmail;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          opacity: .4,
+          fit: BoxFit.cover,
+          image: AssetImage('assets/fondo.jpg'),
+        ),
+      ),
+      child: Row(
+        children: [
+          Expanded(child: imgLogo),
+          Expanded(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 450.0,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      txtEmail,
+                      spaceHorizont,
+                      txtPass,
+                      spaceHorizont,
+                      btnEmail,
+                      spaceHorizont,
+                      btngoogle,
+                      spaceHorizont,
+                      btnface,
+                      spaceHorizont,
+                      btngit,
+                      spaceHorizont,
+                      txtRegister
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
